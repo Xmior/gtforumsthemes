7c31dc270edb7fc0115089367a2dd12cff2bfa08
Xmior <caferiusgrowtopia@gmail.com>
2024-09-25 21:29:35 +0300

    Update content.js

--- content.js
+++ content.js
@@ -2,26 +2,25 @@ chrome.storage.sync.get("selectedTheme", function(data) {
     if (data.selectedTheme) {
         const existingLink = document.querySelector("link[rel='stylesheet']");
         if (existingLink) {
-            existingLink.href = data.selectedTheme; // Seçilen tema CSS dosyasının yolu
+            existingLink.href = data.selectedTheme;
         } else {
             const link = document.createElement('link');
             link.rel = 'stylesheet';
-            link.href = data.selectedTheme; // Seçilen tema CSS dosyasının yolu
+            link.href = data.selectedTheme;
             document.head.appendChild(link);
         }
     }
 });
 
-// Dinle, eğer popup.js'den bir mesaj alırsak
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.theme) {
         const existingLink = document.querySelector("link[rel='stylesheet']");
         if (existingLink) {
-            existingLink.href = request.theme; // Seçilen tema CSS dosyasının yolu
+            existingLink.href = request.theme;
         } else {
             const link = document.createElement('link');
             link.rel = 'stylesheet';
-            link.href = request.theme; // Seçilen tema CSS dosyasının yolu
+            link.href = request.theme;
             document.head.appendChild(link);
         }
         sendResponse({ status: "success" });
