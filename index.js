e030b9cd58729d23437d82f5264c9accbf668720
Xmior <caferiusgrowtopia@gmail.com>
2024-09-25 21:30:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-// Forum bağlantılarını tanımla
 const href = document.querySelectorAll("td.cell-forum > div.forum-wrapper > div > a");
 const hrefTarget = document.querySelectorAll("td.cell-forum");
 
 for (let index = 0; index < hrefTarget.length; index++) {
-    if (href[index]) { // null kontrolü
+    if (href[index]) { // null control bruh
         hrefTarget[index].setAttribute("onclick", "window.location='" + href[index].href + "'");
     }
 }
 
-// Konu bağlantılarını tanımla
 const topicLink = document.querySelectorAll(".topic-item .topic-wrapper .topic-title");
 const topicTarget = document.querySelectorAll(".cell-topic");
 
 for (let index = 0; index < topicTarget.length; index++) {
-    if (topicLink[index]) { // null kontrolü
+    if (topicLink[index]) { // null control again
         topicTarget[index].setAttribute("onclick", "window.location='" + topicLink[index].href + "'");
     }
 }
